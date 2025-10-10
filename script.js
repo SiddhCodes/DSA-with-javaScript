2da12692faec29642e526141642225c746f53a29
Siddhant_Mul <siddhantmul18@gmail.com>
2025-10-10 19:01:22 +0530

    All ones to left and all zeroes to right

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// All zeroes to left and all ones to right
+// All ones to left and all zeroes to right
 let arr = [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0];
 let i = 0;
 let j = 0;
 while (i < arr.length) {
-  if (arr[i] === 0) {
+  if (arr[i] === 1) {
     let temp = arr[i];
     arr[i] = arr[j];
     arr[j] = temp;
