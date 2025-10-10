4e80d3176a23b2c8b4011a413009a44872f02608
Siddhant_Mul <siddhantmul18@gmail.com>
2025-10-10 17:42:44 +0530

    Second max element from array

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-// Max element from array
+// Second max element from array
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 let maxE = Math.max(arr[0], arr[1]);
+let sMaxE = Math.min(arr[0], arr[1]);
 for (let i = 2; i < arr.length; i++) {
   if (arr[i] > maxE) {
+    sMaxE = maxE;
     maxE = arr[i];
+  } else if (arr[i] > sMaxE && arr[i] !== sMaxE) {
+    sMaxE = arr[i];
   }
 }
-console.log(maxE);
+console.log(`max element is: ${maxE} and Second max element is: ${sMaxE}`);
