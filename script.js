7c5eb32cf5f6d04fb6e69cba6db431f1186945d7
Siddhant_Mul <siddhantmul18@gmail.com>
2025-10-11 21:25:19 +0530

    Array right Rotation by 1

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Array left Rotation by 1
+// Array right Rotation by 1
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let temp = arr[0];
-for (let i = 0; i < arr.length - 1; i++) {
-  arr[i] = arr[i + 1];
+let temp = arr[arr.length - 1];
+for (let i = arr.length - 1; i > 0; i--) {
+  arr[i] = arr[i - 1];
 }
-arr[arr.length - 1] = temp;
+arr[0] = temp;
 console.log(arr);
