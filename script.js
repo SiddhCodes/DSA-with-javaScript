d8dcbf883892362d38ddfc6d0a13a675f478087c
Siddhant_Mul <siddhantmul18@gmail.com>
2025-10-12 16:37:18 +0530

    Linear Search an array - If element found print the index else -1

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-// Array left rotation by K elements
+//  Linear Search an array - If element found print the index else -1
 var prompt = require("prompt-sync")();
-let k = Number(prompt("Enter the number of rotations:"));
+
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-for (let i = 0; i < k; i++) {
-  let copy = arr[0];
-  for (let j = 0; j < arr.length - 1; j++) {
-    arr[j] = arr[j + 1];
+let userN = Number(prompt("Enter the number to find its index: "));
+
+let index = -1;
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] === userN) {
+    index = i;
+    break;
   }
-  arr[arr.length - 1] = copy;
 }
-console.log(arr);
+
+if (index !== -1) {
+  console.log(`The number ${userN} is found at index ${index} in the array.`);
+} else {
+  console.log(`The number ${userN} is not present in the array.`);
+}
