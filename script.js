af97a6e55f596d9954287236a96969ad4e761493
Siddhant_Mul <siddhantmul18@gmail.com>
2025-10-16 21:35:48 +0530

    selection sort

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-// Bubble sort
+// selection sort
 let arr = [3, 8, 9, 1, 4, 7, 2, 5, 6];
 
-let n = arr.length;
-
-for (let i = 0; i < n - 1; i++) {
-  for (let j = 0; j < n - 1 - i; j++) {
-    if (arr[j] > arr[j + 1]) {
-      let temp = arr[j];
-      arr[j] = arr[j + 1];
-      arr[j + 1] = temp;
+for (let i = 0; i < arr.length - 1; i++) {
+  let small = i;
+  for (let j = i + 1; j < arr.length; j++) {
+    if (arr[j] < arr[small]) {
+      small = j;
     }
+  }
+  if (i != small) {
+    let temp = arr[i];
+    arr[i] = arr[small];
+    arr[small] = temp;
   }
 }
 
