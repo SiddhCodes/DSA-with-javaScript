2bd44906cabbe743dab6b4a2a6d32c59cb21ebd5
Siddhant_Mul <siddhantmul18@gmail.com>
2025-10-16 21:45:42 +0530

    Insertion sort

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-// selection sort
+// Insertion sort
 let arr = [3, 8, 9, 1, 4, 7, 2, 5, 6];
 
-for (let i = 0; i < arr.length - 1; i++) {
-  let small = i;
-  for (let j = i + 1; j < arr.length; j++) {
-    if (arr[j] < arr[small]) {
-      small = j;
-    }
+for (let i = 1; i < arr.length; i++) {
+  let key = arr[i];
+  let j = i - 1;
+  while (j >= 0 && arr[j] > key) {
+    arr[j + 1] = arr[j];
+    j--;
   }
-  if (i != small) {
-    let temp = arr[i];
-    arr[i] = arr[small];
-    arr[small] = temp;
-  }
+  arr[j + 1] = key;
 }
 
 console.log(arr);
